d423c3d8873b4769d33a3598df98eac7e405816f
edodoe15 <edin.kohnic.20@size.ba>
2023-04-23 16:13:09 +0200

    Performance model

--- PerformanceTab/models.py
+++ PerformanceTab/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Performance (models.Model):
+
+    project_name = models.CharField(max_length=100)
+    project_value = models.FloatField()
+    team_size = models.IntegerField()
+    velocity = models.IntegerField()
+    weeks_over_ddl = models.IntegerField()
+    hourly_price = models.IntegerField()
+    year = models.IntegerField()
