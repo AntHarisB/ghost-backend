02457c5e311321ce5be7b9f7ca51b44da5017c63
edodoe15 <edin.kohnic.20@size.ba>
2023-04-26 16:37:57 +0200

    Unused code deleted, requirements.txt file created, .env created and added to gitignore

--- loginAPI/serializers.py
+++ loginAPI/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
 
--- loginAPI/views.py
+++ loginAPI/views.py
@@ -1,22 +1,17 @@
 from rest_framework import viewsets
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny
 from django.contrib.auth import get_user_model
+from django.conf import settings
 from .serializers import UserSerializer
 from django.contrib.auth import get_user_model
+from rest_framework.authentication import TokenAuthentication
 
 
 class UserViewSet(viewsets.ModelViewSet):
     permission_classes = (AllowAny,)
+    authentication_classes = (TokenAuthentication,)
     serializer_class = UserSerializer
     queryset = get_user_model().objects.all()
 
 
-    # User = get_user_model()
 
-    # user = User.objects.create_user(
-    #     username='johndoe',
-    #     email='johndoe@.com',
-    #     password='abc123456'
-    # )
-
-    # user.save()
