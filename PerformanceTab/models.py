9a3384bd49465255f54cdcf6b5255b63f8948434
edodoe15 <edin.kohnic.20@size.ba>
2023-04-27 13:40:10 +0200

    model created, table in database created

--- PerformanceTab/models.py
+++ PerformanceTab/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Performance (models.Model):
+class ProjectInformation (models.Model):
 
     project_name = models.CharField(max_length=100)
     project_value = models.FloatField()
@@ -9,3 +9,8 @@ class Performance (models.Model):
     weeks_over_ddl = models.IntegerField()
     hourly_price = models.IntegerField()
     year = models.IntegerField()
+    lead_closing = models.FloatField()
+    project_creation = models.CharField(max_length=100)
+    type_of_project = models.CharField(max_length=100)
+    hours_available = models.IntegerField()
+    hours_billed = models.IntegerField()
