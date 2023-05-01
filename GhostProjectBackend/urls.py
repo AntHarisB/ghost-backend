94cd2bb36587d6b46caa9c8ec09c8f80238fc2d3
edodoe15 <edin.kohnic.20@size.ba>
2023-05-01 11:33:22 +0200

    endpoint for hours overview created - view + serializer

--- GhostProjectBackend/urls.py
+++ GhostProjectBackend/urls.py
@@ -3,10 +3,12 @@ from django.urls import path, include
 from PerformanceTab.view_stats import ProjectStats
 from PerformanceTab.view_projectcreation import ProjectCreationCount
 from PerformanceTab.view_typeofproject import ProjectTypeCount
+from PerformanceTab.views_hours import ProjectHours
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('project-statistics/<int:year>/', ProjectStats.as_view(), name='project-stats'),
     path('projectcreation-count/<int:year>/', ProjectCreationCount.as_view(), name='projectcreation-count'),
     path('projecttype-count/<int:year>/', ProjectTypeCount.as_view(), name='projecttype-count'),
+    path('project-hours/<int:year>/', ProjectHours.as_view(), name='project-hours'),
 ]
