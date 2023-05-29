2e3962ecfe0fd20e8c7cfa49b327204cd056d960
edodoe15 <edin.kohnic.20@size.ba>
2023-05-29 09:36:06 +0200

    endpoint protected - only auth user has access

--- Projects/projects_view.py
+++ Projects/projects_view.py
@@ -2,12 +2,10 @@ from rest_framework import generics
 from PerformanceTab.models import ProjectInformation
 from .projects_serializer import ProjectInfoSerializer
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from .models import Profile
 
 class ProjectInfo(generics.ListAPIView):
     serializer_class = ProjectInfoSerializer
-    # permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
     queryset = ProjectInformation.objects.all()
 
     
