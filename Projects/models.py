e8eedd62e78a71e1460a072b8666465a3769b3db
edodoe15 <edin.kohnic.20@size.ba>
2023-06-15 14:03:00 +0200

    new fields added - User model

--- Projects/models.py
+++ Projects/models.py
@@ -4,4 +4,6 @@ from django.db import models
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_photo = models.URLField(blank=True, null=True)
-
+    department = models.CharField(max_length=100, blank=True)
+    monthly_salary = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
+    tech_stack = models.CharField(max_length=100, blank=True)
