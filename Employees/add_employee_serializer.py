02967dda1cc43dd912021ae433c814074e76a89b
edodoe15 <edin.kohnic.20@size.ba>
2023-06-16 18:31:39 +0200

    endpoint for creating new employee

--- Employees/add_employee_serializer.py
+++ Employees/add_employee_serializer.py
@@ -3,12 +3,10 @@ from django.contrib.auth.models import User
 from Projects.models import Profile
 from django.utils.text import slugify
 
-from django.utils.text import slugify
-
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Profile
-        fields = ('department', 'monthly_salary', 'tech_stack', 'profile_photo')
+       model = Profile
+       fields = ('department', 'monthly_salary', 'tech_stack', 'profile_photo')
 
 class AddUserSerializer(serializers.ModelSerializer):
     profile = ProfileSerializer()
@@ -19,19 +17,26 @@ class AddUserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         profile_data = validated_data.pop('profile')
-        profile_serializer = ProfileSerializer(data=profile_data)
-        if profile_serializer.is_valid():
-            first_name = validated_data.get('first_name', '')
-            last_name = validated_data.get('last_name', '')
-            username = slugify(first_name + last_name)
-            
-            user = User.objects.create(username=username, **validated_data)
-            
-            profile = profile_serializer.save(user=user.instance)
-            
-            return user
-        else:
-            raise serializers.ValidationError(profile_serializer.errors)
+
+        first_name = validated_data.get('first_name', '')
+        last_name = validated_data.get('last_name', '')
+        username = slugify(first_name + last_name)
+    
+        user = User.objects.create(username=username, **validated_data)
+        Profile.objects.create(user=user, **profile_data)
+        return user
+
+
+        
+    
+
+
+
+
+
+
+
+
 
 
 
@@ -45,13 +50,3 @@ class AddUserSerializer(serializers.ModelSerializer):
 
 
 
-{
-"first_name":"Tea",
-"last_name":"Tea",
-"profile": {
-    "department":"Design",
-"tech_stack":"UX/UI",
-"monthly_salary":"1500.00"
-}
-
-}
--- Employees/add_employee_view.py
+++ Employees/add_employee_view.py
@@ -1,13 +1,14 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
-from .add_employee_serializer import AddUserSerializer
+from .add_employee_serializer import AddUserSerializer, ProfileSerializer
 from django.contrib.auth.models import User
 from rest_framework import status
-from rest_framework.permissions import DjangoModelPermissions
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework import permissions
 from rest_framework.decorators import permission_classes
+from rest_framework import request
+
 
 @permission_classes((permissions.AllowAny,))
 class EmployeeAddView(APIView):
@@ -16,17 +17,22 @@ class EmployeeAddView(APIView):
             'first_name': request.data.get('first_name'), 
             'last_name': request.data.get('last_name'), 
             'profile': {
-                'profile_photo': request.data.get('profile.profile_photo'),
-                'department': request.data.get('profile.department'), 
-                'monthly_salary': request.data.get('profile.monthly_salary'), 
-                'tech_stack': request.data.get('profile.tech_stack')
+                'profile_photo': request.data.get('profile_photo'),
+                'department': request.data.get('department'), 
+                'monthly_salary': request.data.get('monthly_salary'), 
+                'tech_stack': request.data.get('tech_stack')
             }
         }
-        
         serializer = AddUserSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
+
 
