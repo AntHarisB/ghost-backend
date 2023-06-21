88a13e6ac9cd11c61589d2059c126b0ecca7b34c
edodoe15 <edin.kohnic.20@size.ba>
2023-06-21 11:28:03 +0200

    Inovicing (get all statistics)

--- Invoicing/models.py
+++ Invoicing/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Invoicing(models.Model):
+    client = models.CharField(max_length=100)
+    industry = models.CharField(max_length=100)
+    total_hours_billed = models.IntegerField()
+    amount_billed = models.FloatField()
+    status = models.CharField(max_length=50)
+    paid = models.CharField(max_length=10)
+    sent = models.CharField(max_length=10)
+    
