f7ff567671e2576ddcca7c97938e3f5e642d4066
edodoe15 <edin.kohnic.20@size.ba>
2023-07-04 18:18:52 +0200

    invoicing pdf - updated

--- Invoicing/pdf_invoicing_view.py
+++ Invoicing/pdf_invoicing_view.py
@@ -4,7 +4,6 @@ from .models import Invoicing
 import datetime
 
 def generate_invoice_pdf(request, invoice_id):
-
     invoice = Invoicing.objects.get(id=invoice_id)
 
     response = HttpResponse(content_type='application/pdf')
@@ -15,13 +14,20 @@ def generate_invoice_pdf(request, invoice_id):
     p.drawString(100, 750, 'Ant Colony')
 
     p.setFont('Helvetica', 12)
-    p.drawString(400, 750, str(datetime.date.today()))
+    p.drawString(440, 750, str(datetime.date.today()))
+
+    p.line(100, 745, 500, 745)
 
     p.setFont('Helvetica', 12)
-    p.drawString(100, 700, f'Client: {invoice.client}')
-    p.drawString(100, 680, f'Total Hours Billed: {invoice.total_hours_billed}')
-    p.drawString(100, 660, f'Amount Billed: {invoice.amount_billed}')
+    p.drawString(100, 690, f'Client: {invoice.client}')
+    p.drawString(100, 670, f'Total Hours Billed: {invoice.total_hours_billed}')
+    p.drawString(100, 650, f'Amount Billed: {invoice.amount_billed}')
+
+    p.setFont('Helvetica', 10)
+    p.drawString(100, 100, 'Ant Colony Team')
+    p.drawString(375, 100, 'IT Services and IT Consulting')
 
     p.save()
     return response
+
 
