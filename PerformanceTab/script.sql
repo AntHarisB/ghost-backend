e2ca5a097f58f19a9ce3e5c92f32c9edf407fe54
edodoe15 <edin.kohnic.20@size.ba>
2023-07-16 09:02:28 +0200

    script.sql updated + list of users added (.txt)

--- PerformanceTab/script.sql
+++ PerformanceTab/script.sql
@@ -1,12 +1,7 @@
+CREATE DATABASE Project;
 USE Project;
 
-SELECT * FROM auth_user;
-SELECT * FROM django_session;
-SELECT * FROM performancetab_projectinformation WHERE year=2023;
-SELECT * FROM performancetab_projectinformation_members;
-
-
-INSERT INTO performancetab_projectinformation (id, project_name, project_value, team_size, velocity, weeks_over_ddl, hourly_price, year, lead_closing, project_creation, type_of_project, hours_available, hours_billed, costs_actual, costs_planned, project_value_planned)
+INSERT INTO performancetab_projectinformation (id, project_name, project_value, team_s, velocity, weeks_over_ddl, hourly_price, year, lead_closing, project_creation, type_of_project, hours_available, hours_billed, costs_actual, costs_planned, project_value_planned)
 VALUES 
 ('1', 'Projekat 1', '152128.49', '4', '5', '17', '72', '2022', '14.08', 'preporuka', 'fixed', '672', '945', '140580.00', '120000.22', '170180.13'),
 ('2', 'Projekat 2', '262867.34', '9', '9', '10', '47', '2019', '17.32', 'prodaja', 'on-going', '635', '532', '2502300.00', '281000.04', '267700.20'),
@@ -24,7 +19,7 @@ VALUES
 ('14', 'Projekat 14', '904936.12', '2', '5', '13', '71', '2023', '14.59', 'partnerstvo', 'on-going', '402', '418', '900170.20', '920180.40', '911900.05'),
 ('15', 'Projekat 15', '35090.71', '4', '6', '6', '21', '2020', '8.65', 'prodaja', 'on-going', '211', '417', '32220.50', '35220.40', '33020.40');
 
-INSERT INTO performancetab_projectinformation (id, project_name, project_value, team_size, velocity, weeks_over_ddl, hourly_price, year, lead_closing, project_creation, type_of_project, hours_available, hours_billed, costs_actual, costs_planned, project_value_planned)
+INSERT INTO performancetab_projectinformation (id, project_name, project_value, team_s, velocity, weeks_over_ddl, hourly_price, year, lead_closing, project_creation, type_of_project, hours_available, hours_billed, costs_actual, costs_planned, project_value_planned)
 VALUES 
 ('16', 'Projekat 16', '235090.81', '6', '3', '5', '21', '2019', '8.85', 'prodaja', 'on-going', '211', '417', '232220.50', '135220.40', '133020.40'),
 ('17', 'Projekat 17', '135090.71', '4', '6', '4', '21', '2019', '6.65', 'partnerstvo', 'fixed', '159', '306', '232220.50', '135220.40', '133020.40'),
@@ -97,13 +92,12 @@ UPDATE performancetab_projectinformation SET date_start = '2022-05-02' WHERE id 
 UPDATE performancetab_projectinformation SET date_end = '2022-06-18' WHERE id = 22;
 UPDATE performancetab_projectinformation SET date_end = '2022-07-11' WHERE id = 23;
 
-
 UPDATE performancetab_projectinformation SET date_start = '2023-04-07' WHERE id = 24;
 UPDATE performancetab_projectinformation SET date_start = '2023-05-08' WHERE id = 25;
 UPDATE performancetab_projectinformation SET date_start = '2023-06-09' WHERE id = 26;
 UPDATE performancetab_projectinformation SET date_end = '2023-06-10' WHERE id = 24;
-UPDATE performancetab_projectinformation SET date_end = NULL WHERE id = 25;
-UPDATE performancetab_projectinformation SET date_end = NULL WHERE id = 26;
+UPDATE performancetab_projectinformation SET date_end = '2023-12-10' WHERE id = 25;
+UPDATE performancetab_projectinformation SET date_end = '2023-12-21' WHERE id = 26;
 
 UPDATE performancetab_projectinformation
 SET development = '1300000.00',
@@ -212,129 +206,6 @@ SET development = CASE
         ELSE other
     END
 WHERE id IN (14, 24, 25, 26);
-
-
-UPDATE performancetab_projectinformation SET date_start = '2023-04-07' WHERE id = 24;
-UPDATE performancetab_projectinformation SET date_start = '2023-05-08' WHERE id = 25;
-UPDATE performancetab_projectinformation SET date_start = '2023-06-09' WHERE id = 26;
-UPDATE performancetab_projectinformation SET date_end = '2023-06-10' WHERE id = 24;
-UPDATE performancetab_projectinformation SET date_end = NULL WHERE id = 25;
-UPDATE performancetab_projectinformation SET date_end = NULL WHERE id = 26;
-
-UPDATE performancetab_projectinformation
-SET development = '1300000.00',
-    design = '2150500.00',
-    other = '31000000.00',
-    total_revenue = '1900000.00',
-    direct = '3900000.00',
-    indirect = '1755600.00',
-    marketing = '7480200.00',
-    HRcost = '287050.00',
-    officeCost = '211100.00',
-    salesCosts = '99850.00',
-    otherCosts = '648.00'
-WHERE id = 5;
-
-
-UPDATE performancetab_projectinformation
-SET development = '1300000.00',
-    design = '2150500.00',
-    other = '31000000.00',
-    total_revenue = '1900000.00',
-    direct = '3900000.00',
-    indirect = '1755600.00',
-    marketing = '7480200.00',
-    HRcost = '287050.00',
-    officeCost = '211100.00',
-    salesCosts = '99850.00',
-    otherCosts = '648.00'
-WHERE id = 5;
-
-UPDATE performancetab_projectinformation
-SET development = CASE
-        WHEN id = 14 THEN '1300000.00'
-        WHEN id = 24 THEN '1500000.00'
-        WHEN id = 25 THEN '1400000.00'
-        WHEN id = 26 THEN '1600000.00'
-        ELSE development
-    END,
-    design = CASE
-        WHEN id = 14 THEN '2150500.00'
-        WHEN id = 24 THEN '2200000.00'
-        WHEN id = 25 THEN '2100000.00'
-        WHEN id = 26 THEN '2300000.00'
-        ELSE design
-    END,
-    other = CASE
-        WHEN id = 14 THEN '31000000.00'
-        WHEN id = 24 THEN '32000000.00'
-        WHEN id = 25 THEN '30000000.00'
-        WHEN id = 26 THEN '33000000.00'
-        ELSE other
-    END,
-    total_revenue = CASE
-        WHEN id = 14 THEN '1900000.00'
-        WHEN id = 24 THEN '2000000.00'
-        WHEN id = 25 THEN '1800000.00'
-        WHEN id = 26 THEN '2100000.00'
-        ELSE total_revenue
-    END,
-    direct = CASE
-        WHEN id = 14 THEN '3650000.00'
-        WHEN id = 24 THEN '4100100.00'
-        WHEN id = 25 THEN '4000500.00'
-        WHEN id = 26 THEN '3960300.00'
-        ELSE other
-    END,
-    indirect = CASE
-        WHEN id = 14 THEN '1650000.00'
-        WHEN id = 24 THEN '1730605.00'
-        WHEN id = 25 THEN '1420300.00'
-        WHEN id = 26 THEN '1690900.00'
-        ELSE other
-    END,
-    marketing = CASE
-        WHEN id = 14 THEN '7960000.00'
-        WHEN id = 24 THEN '8350000.00'
-        WHEN id = 25 THEN '8102200.00'
-        WHEN id = 26 THEN '7900800.00'
-        ELSE other
-    END,
-    HRcost = CASE
-        WHEN id = 14 THEN '460800.00'
-        WHEN id = 24 THEN '450200.00'
-        WHEN id = 25 THEN '510050.00'
-        WHEN id = 26 THEN '500200.00'
-        ELSE other
-    END,
-    officeCost = CASE
-        WHEN id = 14 THEN '98600.00'
-        WHEN id = 24 THEN '101200.00'
-        WHEN id = 25 THEN '100500.00'
-        WHEN id = 26 THEN '97050.00'
-        ELSE other
-    END,
-    salesCosts = CASE
-        WHEN id = 14 THEN '53000.00'
-        WHEN id = 24 THEN '51200.00'
-        WHEN id = 25 THEN '52630.00'
-        WHEN id = 26 THEN '49800.00'
-        ELSE other
-    END,
-    otherCosts = CASE
-        WHEN id = 14 THEN '750.00'
-        WHEN id = 24 THEN '610.00'
-        WHEN id = 25 THEN '930.00'
-        WHEN id = 26 THEN '775.00'
-        ELSE other
-    END
-WHERE id IN (14, 24, 25, 26);
-
-INSERT INTO auth_user (username, password, email, is_superuser, first_name, last_name, is_staff, is_active, date_joined)
-VALUES ('user1', 'user1', 'user1@example.com', 0, 'user1', 'user1', 1, 0, now());
-
-INSERT INTO auth_user (username, password, email, is_superuser, first_name, last_name, is_staff, is_active, date_joined)
-VALUES ('user3', SHA2('user3', 256), 'user3@example.com', 0, 'user3', 'user3', 1, 0, NOW());
 
 UPDATE performancetab_projectinformation SET description = 'projekat broj 1' WHERE id = 1;
 UPDATE performancetab_projectinformation SET status = 'Active' WHERE id = 1;
@@ -389,137 +260,226 @@ UPDATE performancetab_projectinformation SET status = 'Active' WHERE id = 25;
 UPDATE performancetab_projectinformation SET description = 'projekat broj 26' WHERE id = 26;
 UPDATE performancetab_projectinformation SET status = 'Inactive' WHERE id = 26;
 
-INSERT INTO performancetab_projectinformation_members (id, projectinformation_id, user_id)
+INSERT INTO revenuecosts_member (id, employment_type, project_id, user_id)
 VALUES 
-(1, 1, 1),
-(2, 1, 3),
-(3, 1, 7),
-(4, 1, 8),
-(5, 2, 2),
-(6, 2, 3),
-(7, 2, 4),
-(8, 2, 5),
-(9, 2, 6),
-(10, 2, 7),
-(11, 2, 8),
-(12, 2, 9),
-(13, 2, 10),
-(14, 3, 12),
-(15, 3, 13),
-(16, 3, 14),
-(17, 3, 15),
-(18, 3, 16),
-(19, 3, 17),
-(20, 3, 18),
-(21, 3, 19),
-(22, 3, 5),
-(23, 3, 6),
-(24, 4, 7),
-(25, 4, 4),
-(26, 4, 6),
-(27, 4, 8),
-(28, 4, 10),
-(29, 5, 3),
-(30, 6, 7),
-(31, 6, 14),
-(32, 7, 13),
-(33, 7, 14),
-(34, 7, 15),
-(35, 7, 16),
-(36, 7, 17),
-(37, 7, 18),
-(38, 7, 19),
-(39, 7, 5),
-(40, 8, 3),
-(41, 8, 6),
-(42, 8, 9),
-(43, 8, 12),
-(44, 9, 7),
-(45, 9, 11),
-(46, 9, 15),
-(47, 10, 4),
-(48, 10, 8),
-(49, 10, 12),
-(50, 10, 14),
-(51, 10, 15),
-(52, 10, 17),
-(53, 11, 4),
-(54, 11, 6),
-(55, 11, 12),
-(56, 11, 14),
-(57, 11, 15),
-(58, 12, 3),
-(59, 12, 6),
-(60, 12, 9),
-(61, 12, 12),
-(62, 13, 13),
-(63, 13, 14),
-(64, 13, 15),
-(65, 13, 16),
-(66, 13, 17),
-(67, 13, 18),
-(68, 13, 19),
-(69, 13, 5),
-(70, 14, 3),
-(71, 14, 6),
-(72, 15, 4),
-(73, 15, 6),
-(74, 15, 8),
-(75, 15, 10),
-(76, 16, 8),
-(77, 16, 9),
-(78, 16, 10),
-(79, 16, 11),
-(80, 16, 12),
-(81, 16, 13),
-(82, 17, 9),
-(83, 17, 6),
-(84, 17, 11),
-(85, 17, 12),
-(86, 18, 13),
-(87, 18, 14),
-(88, 18, 15),
-(89, 18, 16),
-(90, 18, 17),
-(91, 18, 18),
-(92, 18, 19),
-(93, 18, 5),
-(94, 19, 12),
-(95, 19, 14),
-(96, 19, 15),
-(97, 19, 17),
-(98, 20, 4),
-(99, 20, 6),
-(100, 20, 12),
-(101, 20, 14),
-(102, 20, 15),
-(103, 21, 1),
-(104, 21, 3),
-(105, 21, 7),
-(106, 21, 8),
-(107, 22, 3),
-(108, 22, 5),
-(109, 22, 6),
-(110, 22, 7),
-(111, 22, 8),
-(112, 22, 9),
-(113, 22, 10),
-(114, 22, 19),
-(115, 23, 15),
-(116, 23, 16),
-(117, 23, 17),
-(118, 23, 18),
-(119, 24, 5),
-(120, 24, 8),
-(121, 24, 12),
-(122, 24, 14),
-(123, 24, 15),
-(124, 24, 17),
-(125, 24, 4),
-(126, 25, 15),
-(127, 25, 1),
-(128, 25, 3),
-(129, 25, 7),
-(130, 26, 9),
-(131, 26, 6),
-(132, 26, 11);
+(1, 'Full-time', 1, 1),
+(2, 'Full-time', 1, 3),
+(3, 'Part-time', 1, 7),
+(4, 'Full-time', 1, 8),
+(5, 'Part-time', 2, 2),
+(6, 'Full-time', 2, 3),
+(7, 'Part-time', 2, 4),
+(8, 'Full-time', 2, 5),
+(9, 'Part-time', 2, 6),
+(10, 'Full-time', 2, 7),
+(11, 'Part-time', 2, 8),
+(12, 'Full-time', 2, 9),
+(13, 'Part-time', 2, 10),
+(14, 'Full-time', 3, 12),
+(15, 'Part-time', 3, 13),
+(16, 'Full-time', 3, 14),
+(17, 'Part-time', 3, 15),
+(18, 'Full-time', 3, 16),
+(19, 'Part-time', 3, 17),
+(20, 'Full-time', 3, 18),
+(21, 'Part-time', 3, 19),
+(22, 'Full-time', 3, 5),
+(23, 'Part-time', 3, 6),
+(24, 'Full-time', 4, 7),
+(25, 'Part-time', 4, 4),
+(26, 'Full-time', 4, 6),
+(27, 'Part-time', 4, 8),
+(28,'Full-time', 4, 10),
+(29, 'Part-time', 5, 3),
+(30, 'Full-time', 6, 7),
+(31, 'Part-time', 6, 14),
+(32, 'Full-time', 7, 13),
+(33, 'Part-time', 7, 14),
+(34, 'Full-time', 7, 15),
+(35, 'Part-time', 7, 16),
+(36, 'Full-time', 7, 17),
+(37, 'Part-time', 7, 18),
+(38, 'Full-time', 7, 19),
+(39, 'Part-time', 7, 5),
+(40, 'Full-time', 8, 3),
+(41, 'Part-time', 8, 6),
+(42, 'Full-time', 8, 9),
+(43, 'Part-time', 8, 12),
+(44, 'Full-time', 9, 7),
+(45, 'Part-time', 9, 11),
+(46, 'Full-time', 9, 15),
+(47, 'Part-time', 10, 4),
+(48, 'Full-time', 10, 8),
+(49, 'Part-time', 10, 12),
+(50,'Full-time', 10, 14),
+(51, 'Part-time', 10, 15),
+(52, 'Full-time', 10, 17),
+(53, 'Part-time', 11, 4),
+(54, 'Full-time', 11, 6),
+(55, 'Part-time', 11, 12),
+(56, 'Full-time', 11, 14),
+(57, 'Part-time', 11, 15),
+(58, 'Full-time', 12, 3),
+(59, 'Part-time', 12, 6),
+(60, 'Full-time', 12, 9),
+(61, 'Part-time', 12, 12),
+(62, 'Full-time', 13, 13),
+(63, 'Part-time', 13, 14),
+(64, 'Full-time', 13, 15),
+(65, 'Part-time', 13, 16),
+(66, 'Full-time', 13, 17),
+(67, 'Part-time', 13, 18),
+(68, 'Full-time', 13, 19),
+(69, 'Part-time', 13, 5),
+(70, 'Full-time', 14, 3),
+(71, 'Part-time', 14, 6),
+(72, 'Full-time', 15, 4),
+(73, 'Part-time', 15, 6),
+(74, 'Full-time', 15, 8),
+(75, 'Part-time', 15, 10),
+(76, 'Full-time', 16, 8),
+(77, 'Part-time', 16, 9),
+(78, 'Full-time' ,16, 10),
+(79, 'Part-time', 16, 11),
+(80, 'Full-time', 16, 12),
+(81, 'Part-time', 16, 13),
+(82, 'Full-time', 17, 9),
+(83, 'Part-time', 17, 6),
+(84, 'Full-time', 17, 11),
+(85, 'Part-time', 17, 12),
+(86, 'Full-time', 18, 13),
+(87, 'Part-time', 18, 14),
+(88, 'Full-time', 18, 15),
+(89, 'Part-time', 18, 16),
+(90, 'Full-time', 18, 17),
+(91, 'Part-time', 18, 18),
+(92, 'Full-time', 18, 19),
+(93, 'Part-time', 18, 5),
+(94, 'Full-time', 19, 12),
+(95, 'Full-time', 19, 14),
+(96, 'Part-time', 19, 15),
+(97, 'Part-time', 19, 17),
+(98, 'Part-time', 20, 4),
+(99, 'Part-time', 20, 6),
+(100, 'Full-time', 20, 12),
+(101, 'Part-time', 20, 14),
+(102, 'Full-time', 20, 15),
+(103, 'Part-time', 21, 1),
+(104, 'Full-time', 21, 3),
+(105, 'Part-time', 21, 7),
+(106, 'Full-time', 21, 8),
+(107, 'Part-time',22, 3),
+(108, 'Part-time', 22, 5),
+(109, 'Full-time', 22, 6),
+(110, 'Full-time', 22, 7),
+(111, 'Part-time', 22, 8),
+(112, 'Full-time', 22, 9),
+(113, 'Part-time', 22, 10),
+(114, 'Full-time', 22, 19),
+(115, 'Part-time', 23, 15),
+(116, 'Full-time', 23, 16),
+(117, 'Part-time', 23, 17),
+(118, 'Full-time', 23, 18),
+(119, 'Part-time', 24, 5),
+(120, 'Full-time', 24, 8),
+(121, 'Part-time', 24, 12),
+(122, 'Full-time', 24, 14),
+(123, 'Part-time', 24, 15),
+(124, 'Full-time', 24, 17),
+(125, 'Part-time', 24, 4),
+(126, 'Full-time', 25, 15),
+(127, 'Part-time', 25, 1),
+(128, 'Full-time', 25, 3),
+(129, 'Part-time', 25, 7),
+(130, 'Full-time', 26, 9),
+(131, 'Part-time', 26, 6),
+(132, 'Full-time', 26, 11);
+
+INSERT INTO projects_profile(id, profile_photo, user_id)
+VALUES
+( 1, 'https://randomuser.me/api/portraits/women/9.jpg', 1),
+( 2, 'https://randomuser.me/api/portraits/men/67.jpg', 2),
+( 3, 'https://randomuser.me/api/portraits/women/58.jpg', 3),
+( 4, 'https://randomuser.me/api/portraits/men/31.jpg', 4),
+( 5, 'https://randomuser.me/api/portraits/men/72.jpg', 5),
+( 6, 'https://randomuser.me/api/portraits/men/59.jpg', 6),
+( 7, 'https://randomuser.me/api/portraits/men/53.jpg', 7),
+( 8, 'https://randomuser.me/api/portraits/men/3.jpg', 8),
+( 9, 'https://randomuser.me/api/portraits/women/74.jpg', 9),
+( 10, 'https://randomuser.me/api/portraits/men/20.jpg', 10),
+( 11, 'https://randomuser.me/api/portraits/men/63.jpg', 11),
+( 12, 'https://randomuser.me/api/portraits/women/50.jpg', 12),
+( 13, 'https://randomuser.me/api/portraits/men/41.jpg', 13),
+( 14, 'https://randomuser.me/api/portraits/men/12.jpg', 14),
+( 15, 'https://randomuser.me/api/portraits/men/64.jpg', 15),
+( 16, 'https://randomuser.me/api/portraits/men/46.jpg', 16),
+( 17, 'https://randomuser.me/api/portraits/men/13.jpg', 17),
+( 18, 'https://randomuser.me/api/portraits/men/72.jpg', 18),
+( 19, 'https://randomuser.me/api/portraits/women/66.jpg', 19);
+
+UPDATE projects_profile
+SET department='Development', monthly_salary='2500.00', tech_stack='Full stack' WHERE id =1;
+UPDATE projects_profile
+SET department='Development', monthly_salary='2500.00', tech_stack='Front end' WHERE id =2;
+UPDATE projects_profile
+SET department='Development', monthly_salary='3500.00', tech_stack='Back end' WHERE id =3;
+UPDATE projects_profile
+SET department='Design', monthly_salary='2800.00', tech_stack='UX/UI' WHERE id =4;
+UPDATE projects_profile
+SET department='Design', monthly_salary='2100.00', tech_stack='UX/UI' WHERE id =5;
+UPDATE projects_profile
+SET department='Design', monthly_salary='1700.00', tech_stack='UX/UI' WHERE id =6;
+UPDATE projects_profile
+SET department='Development', monthly_salary='1700.00', tech_stack='Full stack' WHERE id =7;
+UPDATE projects_profile
+SET department='Development', monthly_salary='195.00', tech_stack='Full stack' WHERE id =8;
+UPDATE projects_profile
+SET department='Development', monthly_salary='2300.00', tech_stack='Front end' WHERE id =9;
+UPDATE projects_profile
+SET department='Management', monthly_salary='2100.00', tech_stack='N/A' WHERE id =10;
+UPDATE projects_profile
+SET department='Management', monthly_salary='2000.00', tech_stack='N/A' WHERE id =11;
+UPDATE projects_profile
+SET department='Administration', monthly_salary='1900.00', tech_stack='N/A' WHERE id =12;
+UPDATE projects_profile
+SET department='Development', monthly_salary='1800.00', tech_stack='Back end' WHERE id =13;
+UPDATE projects_profile
+SET department='Administration', monthly_salary='2450.00', tech_stack='N/A' WHERE id =14;
+UPDATE projects_profile
+SET department='Development', monthly_salary='1750.00', tech_stack='Back end' WHERE id =15;
+UPDATE projects_profile
+SET department='Development', monthly_salary='1770.00', tech_stack='Front end' WHERE id =16;
+UPDATE projects_profile
+SET department='Design', monthly_salary='2210.00', tech_stack='UX/UI' WHERE id =17;
+UPDATE projects_profile
+SET department='Development', monthly_salary='1880.00', tech_stack='Full stack' WHERE id =18;
+UPDATE projects_profile
+SET department='Design', monthly_salary='240.00', tech_stack='UX/UI' WHERE id =19;
+
+INSERT INTO invoicing_invoicing(id, client, industry, total_hours_billed, amount_billed, status, paid, sent)
+VALUES
+('1', 'Gerlach-Mills', 'Tools', '1863', '126000.45', 'sent', 'unpaid', 'sent'),
+('2', 'Cartwright-Veum', 'Garden', '32103', '160000.08', 'paid', 'paid', 'sent'),
+('3', 'Donnelly-Heidenreich', 'Health', '89384', '319000.76', 'paid', 'paid', 'sent'),
+('4', 'Hyatt-Trantow', 'Health', '87105', '823000.36', 'paid', 'paid', 'sent'),
+('5', 'Lind Inc', 'Electronics', '39883', '273000.36', 'sent', 'unpaid', 'sent'),
+('6', 'Hartmann LLC', 'Baby', '24919', '532000.60', 'sent', 'unpaid', 'sent'),
+('7', 'Gottlieb Group', 'Home', '21828', '685000.33', 'paid', 'unpaid', 'sent'),
+('8', 'Metz-Schmitt', 'Clothing', '76945', '532000.60', 'not sent', 'unpaid', 'sent'),
+('9', 'Monahan and Sons', 'Home', '42438', '284000.60', 'not sent', 'unpaid', 'sent'),
+('10', 'Breitenberg Inc', 'Books', '28729', '762000.62', 'not sent', 'paid', 'not sent'),
+('11', 'Gerlach-Mills', 'Tools', '1863', '126000.45', 'sent', 'unpaid', 'sent'),
+('12', 'Cartwright-Veum', 'Garden', '32103', '160000.08', 'paid', 'paid', 'sent'),
+('13', 'Donnelly-Heidenreich', 'Health', '89384', '319000.76', 'paid', 'paid', 'sent'),
+('14', 'Hyatt-Trantow', 'Health', '87105', '823000.36', 'paid', 'paid', 'sent'),
+('15', 'Lind Inc', 'Electronics', '39883', '273000.36', 'sent', 'unpaid', 'sent'),
+('16', 'Hartmann LLC', 'Baby', '24919', '532000.60', 'sent', 'unpaid', 'sent'),
+('17', 'Gottlieb Group', 'Home', '21828', '685000.33', 'paid', 'unpaid', 'sent'),
+('18', 'Metz-Schmitt', 'Clothing', '76945', '532000.60', 'not sent', 'unpaid', 'sent'),
+('19', 'Monahan and Sons', 'Home', '42438', '284000.60', 'not sent', 'unpaid', 'sent'),
+('20', 'Breitenberg Inc', 'Books', '28729', '762000.62', 'not sent', 'paid', 'not sent'),
+('21', 'Gerlach-Mills', 'Tools', '1863', '126000.45', 'sent', 'unpaid', 'sent'),
+('22', 'Cartwright-Veum', 'Garden', '32103', '160000.08', 'paid', 'paid', 'sent'),
+('23', 'Donnelly-Heidenreich', 'Health', '89384', '319000.76', 'paid', 'paid', 'sent'),
+('24', 'Hyatt-Trantow', 'Health', '87105', '823000.36', 'paid', 'paid', 'sent'),
+('25', 'Lind Inc', 'Electronics', '39883', '273000.36', 'sent', 'unpaid', 'sent');
